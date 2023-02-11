6a182685d81951e3516e185f35755e5c672374df
RafaelSRV <rafael-srv@hotmail.com>
2023-02-10 23:15:19 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import * as C from './App.styles'
 const App = () =>{
   return(
     <C.Container>
-      
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+      </C.Area>
     </C.Container>
 
   );
