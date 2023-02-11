99a03941a70864c9cd5f8577d108128acc93dafa
RafaelSRV <rafael-srv@hotmail.com>
2023-02-11 00:37:48 -0300

    Salvando texto dos inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import * as C from './App.styles'
+import { useState } from 'react';
+import { Item } from './types/Item'
+import { ListItem } from './components/ListItem';
+import { AddArea} from './components/AddArea'
+
 
 const App = () =>{
+
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name:"Comprar o pão", done: false},
+    {id: 2, name:"Comprar o sabão", done: false}
+  ])
+
+  const handleAddTask = (taskName: string)=> {
+    let newList = [...list];
+    newList.push({
+      id: list.length +1,
+      name: taskName,
+      done: false
+    })
+    setList(newList)
+  }
   return(
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+        
+        <AddArea onEnter={handleAddTask} />
+        
+        {list.map((item, index)=>(
+          <ListItem key={index} item={item}></ListItem>
+        ))}
       </C.Area>
     </C.Container>
 
