e1801e4b2a37622bd86b6b3debf93a60f39e3a67
RafaelSRV <rafael-srv@hotmail.com>
2023-02-17 23:49:04 -0300

    Subindo projeto

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -11,7 +11,7 @@ export const Container = styled.div(({ done } : ContainerProps)=>(
     display: flex;
     background-color: #20212C;
     padding: 10px;
-    border-radius: 10px;
+    border-radius: 11px;
     margin-bottom: 10px;
     align-items: center;
     
