4aca574eabb67d79945477cb38eff18c54bcd826
RafaelSRV <rafael-srv@hotmail.com>
2023-02-24 23:56:11 -0300

    Subindo projeto

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -15,7 +15,7 @@ export const Container = styled.div(({ done } : ContainerProps)=>(
     margin-bottom: 12px;
     align-items: center;
     content-align: center;
-    border: solid 1px;
+    border: solid 2px;
     
     label{
         color: #CCC;
